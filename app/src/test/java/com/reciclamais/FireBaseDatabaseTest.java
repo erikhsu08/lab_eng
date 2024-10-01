e8a4d9b7d14878521963274eaa1cb50aeab266ed
erikhsu08 <erikhsu08@gmail.com>
2024-10-01 13:51:17 -0300

    corrigindo workflow e testes

--- app/src/test/java/com/reciclamais/FireBaseDatabaseTest.java
+++ app/src/test/java/com/reciclamais/FireBaseDatabaseTest.java
@@ -1,42 +1,37 @@
 package com.reciclamais;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.reciclamais.model.Produto;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
 public class FireBaseDatabaseTest {
 
     private DatabaseReference databaseReference;
 
     @Before
     public void setUp() {
-        // Inicializa o Firebase Realtime Database
+        // Inicializa o Firebase no contexto de teste
+        FirebaseApp.initializeApp(InstrumentationRegistry.getInstrumentation().getTargetContext());
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("produtos_test");
+        databaseReference = firebaseDatabase.getReference("produtos");
     }
 
     @Test
     public void adicionarProdutoAoFirebase() {
-        // Cria um novo produto
-        Produto produto = new Produto("Foguete", "Fácil", R.drawable.foguete);
-
-        // Adiciona o produto ao Firebase
+        Produto produto = new Produto("Foguete", "Facil", R.drawable.foguete);
         databaseReference.push().setValue(produto).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                System.out.println("Produto adicionado com sucesso.");
-            } else {
-                System.err.println("Erro ao adicionar o produto.");
-            }
+            assertTrue("Falha ao adicionar o produto ao Firebase", task.isSuccessful());
         });
-
-        // Aguarda até que a operação esteja completa
-        try {
-            Thread.sleep(3000); // Tempo para garantir a execução do listener
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
