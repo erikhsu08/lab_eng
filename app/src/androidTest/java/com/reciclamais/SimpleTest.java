675ab74bb11aa901b8c4a9fd40470a9daf94a3e3
erikhsu08 <erikhsu08@gmail.com>
2024-10-02 00:12:54 -0300

    Add teste unitario criar foguete

---
+++ app/src/androidTest/java/com/reciclamais/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.reciclamais;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimpleTest {
+    @Test
+    public void testSimple() {
+        assertTrue(true);
+    }
+}
